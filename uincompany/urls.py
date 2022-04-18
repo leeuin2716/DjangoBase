10823a06b3926d8933c30c7ff318d859c69cd7ed
이유인 <leeuin2716>
2022-04-18 20:53:36 +0900

    ㅇ

--- uincompany/urls.py
+++ uincompany/urls.py
@@ -2,9 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('', TemplateView.as_view(template_name='root.html'), name='root'),
+
 ]
 
 if settings.DEBUG:
@@ -12,6 +16,7 @@ if settings.DEBUG:
 
     urlpatterns += [
         path('__debug__/', include('debug_toolbar.urls')),
+
     ]
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
